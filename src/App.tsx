dcb9223a53d00affa25524797acfc24bc87cb889
Patrick Santos <patrickspteixeira@gmail.com>
2021-06-22 00:04:52 -0300

    add structure and styles for homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import Home from './pages/Home'
 
 function App() {
   return (
-    <h1>Hello world</h1>
+    <Home />
   )
 }
 
---
+++ src/components/Button.tsx
@@ -0,0 +1,14 @@
+import React, { ButtonHTMLAttributes } from 'react'
+import '../styles/button.scss'
+
+type Props = ButtonHTMLAttributes<HTMLButtonElement>
+
+function Button({ children, ...rest }: Props) {
+    return (
+        <button className="button" {...rest}>
+            {children}
+        </button>
+    )
+}
+
+export default Button
